3b2bab396744c8c1f77ddc4c6ef7ed94419f5fc2
trecain <developedbycain@yahoo.com>
2018-10-08 13:29:12 -0500

    finished the assignment with comments

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -6,7 +6,36 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!");
+            // Main function used to call all the methods in the program.
+            Console.WriteLine(Add(9, 10));
+            Console.WriteLine(Subtract(99, 10));
+            Console.WriteLine(Multiply(55, 70));
+            Console.WriteLine(Divide(334, 140));
+            Console.ReadLine();
+        }
+
+        static decimal Add(decimal numOne, decimal numTwo)
+        {
+            // Takes in to decimals as parameters and returns the sum.
+            return numOne + numTwo;
+        }
+
+        static decimal Subtract(decimal numOne, decimal numTwo)
+        {
+            // Takes in to decimals as parameters and submits the remainder. 
+            return numOne - numTwo;
+        }
+
+        static decimal Multiply (decimal numOne, decimal numTwo)
+        {
+            //  Takes in to decimals as parameters and multiplies the two.
+            return numOne * numTwo;
+        }
+
+        static decimal Divide (decimal numOne, decimal numTwo)
+        {
+            // Takes in two decimals as parameters and returns the quotient of the two. 
+            return numOne / numTwo;
         }
     }
 }
